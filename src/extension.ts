a5de30f86cce706aaa4bcf745402bed57903f67e
Renato Menezes Machado <renatormm@hotmail.com>
2022-12-22 21:26:27 -0300

    feat: create tree data provider for runs

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,7 @@ import * as vscode from 'vscode';
 
 import { ContestProvider, Contest } from './contest';
 import { ProblemProvider, Problem } from './problem';
+import { RunProvider, Run } from './run';
 
 export function activate(context: vscode.ExtensionContext) {
 	const rootPath = (vscode.workspace.workspaceFolders && (vscode.workspace.workspaceFolders.length > 0))
@@ -18,8 +19,14 @@ export function activate(context: vscode.ExtensionContext) {
 	const problemProvider = new ProblemProvider(rootPath);
 	vscode.window.registerTreeDataProvider('problems', problemProvider);
 
+	const runProvider = new RunProvider(rootPath);
+	vscode.window.registerTreeDataProvider('runs', runProvider);
+	
 	vscode.commands.registerCommand('contests.selectContest', contestNumber => {
 		problemProvider.refresh(contestNumber);
+	});
+	vscode.commands.registerCommand('problems.selectProblem', problemNumber => {
+		runProvider.refresh(problemNumber);
 	});
 	vscode.commands.registerCommand('problems.downloadProblem', (problem: Problem) => {
 		problemProvider.downloadProblemToWorkspace(problem);
---
+++ src/run.ts
@@ -0,0 +1,89 @@
+import * as vscode from 'vscode';
+import * as path from 'path';
+import axios from 'axios';
+import { treeFileDecorationProvider } from './treeFileDecorationProvider';
+
+export class RunProvider implements vscode.TreeDataProvider<Run> {
+
+	private _problemNumber = 0;
+	private _onDidChangeTreeData: vscode.EventEmitter<Run | undefined | void> = new vscode.EventEmitter<Run | undefined | void>();
+	readonly onDidChangeTreeData: vscode.Event<Run | undefined | void> = this._onDidChangeTreeData.event;
+
+	constructor(private workspaceRoot: string | undefined) {
+	}
+
+	refresh(problemNumber: number): void {
+		this._problemNumber = problemNumber;
+		this._onDidChangeTreeData.fire();
+	}
+
+	getTreeItem(element: Run): vscode.TreeItem {
+		return element;
+	}
+
+	getChildren(): Thenable<Run[]> {
+		if (!this.workspaceRoot) {
+			vscode.window.showInformationMessage('No run in empty workspace');
+			return Promise.resolve([]);
+		}
+
+		return Promise.resolve(this._getRuns());
+	}
+
+	private async _getRuns(): Promise<Run[]> {
+		if (this._problemNumber === 0) {
+			return [];
+		}
+		const apiAddress = process.env.apiAddress;
+		try {
+			const response = await axios({
+				method: 'get',
+				url: apiAddress + '/problem/' + this._problemNumber + '/run',
+			});
+			return (response.data || [])
+				.filter((run: any) => run.usernumber === 3151)
+				.sort((r1: any, r2: any) => r1.rundate - r2.rundate)
+				.map((run: any) => this._toRun(run));
+		} catch (error) {
+			vscode.window.showErrorMessage('Fecthing runs failed');
+			console.error(error);
+			return [];
+		}
+	}
+
+	private _toRun(run: any) {
+		return new Run(
+			run.runfilename,
+			run.autoanswer.split(')').pop().trim(),
+			run.runanswer === 1,
+			`${run.contestnumber}@${run.runsitenumber}@${run.runnumber}`,
+			vscode.TreeItemCollapsibleState.None
+		);
+	}
+}
+
+export class Run extends vscode.TreeItem {
+
+	constructor(
+		public readonly label: string,
+		private readonly descriptionText: string,
+		success: boolean,
+		uri: string,
+		public readonly collapsibleState: vscode.TreeItemCollapsibleState,
+		public readonly command?: vscode.Command,
+	) {
+		super(label, collapsibleState);
+
+		this.description = this.descriptionText;
+		this.resourceUri = vscode.Uri.parse(uri);
+		this.tooltip = this.label;
+		treeFileDecorationProvider.updateRunDecorator(this.resourceUri, success, this.description);
+	}
+
+	iconPath = {
+		light: path.join(__filename, '..', '..', 'resources', 'light', 'run.svg'),
+		dark: path.join(__filename, '..', '..', 'resources', 'dark', 'run.svg')
+	};
+
+	contextValue = 'run';
+}
