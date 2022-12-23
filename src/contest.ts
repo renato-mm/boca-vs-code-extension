1d2b03326e417adbf7d46fa4fab03a7fbdd995bf
Renato Menezes Machado <renatormm@hotmail.com>
2022-12-22 21:21:30 -0300

    feat: create tree data provider for contests

---
+++ src/contest.ts
@@ -0,0 +1,74 @@
+import * as vscode from 'vscode';
+import * as path from 'path';
+import axios from 'axios';
+
+export class ContestProvider implements vscode.TreeDataProvider<Contest> {
+
+	private _onDidChangeTreeData: vscode.EventEmitter<Contest | undefined | void> = new vscode.EventEmitter<Contest | undefined | void>();
+	readonly onDidChangeTreeData: vscode.Event<Contest | undefined | void> = this._onDidChangeTreeData.event;
+
+	constructor(private workspaceRoot: string | undefined) {
+	}
+
+	refresh(): void {
+		this._onDidChangeTreeData.fire();
+	}
+
+	getTreeItem(element: Contest): vscode.TreeItem {
+		return element;
+	}
+
+	getChildren(): Thenable<Contest[]> {
+		if (!this.workspaceRoot) {
+			vscode.window.showInformationMessage('No contest in empty workspace');
+			return Promise.resolve([]);
+		}
+
+		return Promise.resolve(this._getContests());
+	}
+
+	private async _getContests(): Promise<Contest[]> {
+		const apiAddress = process.env.apiAddress;
+		try {
+			const response = await axios({
+				method: 'get',
+				url: apiAddress + '/contests',
+			});
+			return (response.data || []).filter((contest: any) => contest.contestnumber !== 0).map((contest: any) => this._toContest(contest));
+		} catch (error) {
+			vscode.window.showErrorMessage('Fecthing contests failed');
+			console.error(error);
+			return [];
+		}
+	}
+
+	private _toContest(contest: any) {
+		return new Contest(contest.contestname, '', vscode.TreeItemCollapsibleState.None, {
+			command: 'contests.selectContest',
+			title: 'Select Contest',
+			arguments: [contest.contestnumber]
+		});
+	}
+}
+
+export class Contest extends vscode.TreeItem {
+
+	constructor(
+		public readonly label: string,
+		private readonly languages: string,
+		public readonly collapsibleState: vscode.TreeItemCollapsibleState,
+		public readonly command?: vscode.Command
+	) {
+		super(label, collapsibleState);
+
+		this.tooltip = this.label;
+		this.description = this.languages;
+	}
+
+	iconPath = {
+		light: path.join(__filename, '..', '..', 'resources', 'light', 'contest.svg'),
+		dark: path.join(__filename, '..', '..', 'resources', 'dark', 'contest.svg')
+	};
+
+	contextValue = 'contest';
+}
--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,18 @@
 import * as vscode from 'vscode';
 
+import { ContestProvider, Contest } from './contest';
 
 export function activate(context: vscode.ExtensionContext) {
+	const rootPath = (vscode.workspace.workspaceFolders && (vscode.workspace.workspaceFolders.length > 0))
+		? vscode.workspace.workspaceFolders[0].uri.fsPath : undefined;
+
+	const contestProvider = new ContestProvider(rootPath);
+	vscode.window.registerTreeDataProvider('contests', contestProvider);
+	vscode.commands.registerCommand('contests.fetchContests', () => contestProvider.refresh());
+	vscode.commands.registerCommand('contests.refreshEntry', () => contestProvider.refresh());
+	vscode.commands.registerCommand('contests.addEntry', () => vscode.window.showInformationMessage(`Successfully called add entry.`));
+	vscode.commands.registerCommand('contests.editEntry', (contest: Contest) => vscode.window.showInformationMessage(`Successfully called edit entry on ${contest.label}.`));
+	vscode.commands.registerCommand('contests.deleteEntry', (contest: Contest) => vscode.window.showInformationMessage(`Successfully called delete entry on ${contest.label}.`));
 }
 
 export function deactivate() {}
