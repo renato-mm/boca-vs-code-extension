b11ee0fcb86561dad63d793602bd7c191e12d23c
Renato Menezes Machado <renatormm@hotmail.com>
2022-12-22 21:11:01 -0300

    feat: create a file decoration provider

---
+++ src/treeFileDecorationProvider.ts
@@ -0,0 +1,66 @@
+import {
+	FileDecorationProvider,
+	FileDecoration,
+	Uri,
+	EventEmitter,
+	Event,
+	CancellationToken,
+	ProviderResult,
+	Disposable,
+	window,
+	ThemeColor
+} from "vscode";
+
+class TreeFileDecorationProvider implements FileDecorationProvider {
+	private _disposables: Array<Disposable> = [];
+	private _fileDecorations: Map<string, FileDecoration> = new Map<string, FileDecoration>();
+
+	constructor() {
+    this._disposables = [];
+    this._disposables.push(window.registerFileDecorationProvider(this));
+  }
+
+	updateProblemDecorator(resourceUri: Uri, solved: boolean, color: string): void {
+		const decorator: FileDecoration = {
+			badge: solved ? '⚑' : '⚐',
+			color: new ThemeColor(`problems.${color}`)
+		};
+		const currrentDecorator = this._fileDecorations.get(resourceUri.toString());
+		if (currrentDecorator?.badge !== decorator.badge) {
+			this._fileDecorations.set(resourceUri.toString(), decorator);
+			this._onDidChangeFileDecorations.fire(resourceUri);
+		}
+	}
+
+	updateRunDecorator(resourceUri: Uri, solved: boolean, tooltip: string): void {
+		const decorator: FileDecoration = solved
+			? {
+				badge: '✓',
+				color: new ThemeColor('runs.correctAnswer'),
+				tooltip
+			}
+			: {
+				badge: '☓',
+				color: new ThemeColor('runs.incorrectAnswer'),
+				tooltip
+			};
+		const currrentDecorator = this._fileDecorations.get(resourceUri.toString());
+		if (currrentDecorator?.badge !== decorator.badge) {
+			this._fileDecorations.set(resourceUri.toString(), decorator);
+			this._onDidChangeFileDecorations.fire(resourceUri);
+		}
+	}
+
+	_onDidChangeFileDecorations: EventEmitter<Uri | Uri[]> = new EventEmitter<Uri | Uri[]>();
+	onDidChangeFileDecorations: Event<Uri | Uri[]> = this._onDidChangeFileDecorations.event;
+
+	provideFileDecoration(uri: Uri, _token: CancellationToken): ProviderResult<FileDecoration> {
+		return this._fileDecorations.get(uri.toString());
+	}
+
+  dispose() {
+    this._disposables.forEach((d) => d.dispose());
+  }
+}
+
+export const treeFileDecorationProvider = new TreeFileDecorationProvider();
